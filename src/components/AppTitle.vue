e65d601e9ee90b6ad5c55831e2e5cce6f7296bce
RuggeroCoco <ruggerococo94@gmail.com>
2023-04-14 19:33:22 +0200

    End of project

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return { title: "La mia prima app con Vite!" };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ this.title }}</h1>
+</template>
+
+<style scoped></style>
